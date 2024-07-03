e8dd5b4ad49ec6a4fd3423c4e291b1ebf902c034
seniorsam9 <rxrld050@hotmail.com>
2024-07-03 23:01:54 +0300

    handling single tokens

--- src/Lexer/Tokenizer.ts
+++ src/Lexer/Tokenizer.ts
@@ -1,17 +1,41 @@
 import { Token } from "./Token";
 import { TokenType } from "./TokenType";
 
-export function tokenize(expr: string) {
+export function tokenize(expr: string): Array<Token> {
   let tokens = new Array<Token>();
   // split the expression by character
   let lexemes = expr.split("");
 
   // recognize each lexeme until EOF
   while (lexemes.length > 0) {
-    if (lexemes[0] === "(") {
-      tokens.push(new Token(lexemes.shift(), TokenType.OpenParen));
-    } else if (lexemes[0] === "(") {
-      tokens.push(new Token(lexemes.shift(), TokenType.CloseParen));
+    /**
+     * HANDLING SINGLE-CHARACTER TOKENS
+     */
+    switch (lexemes[0]) {
+      case "(":
+        tokens.push(new Token(lexemes.shift(), TokenType.OpenParen));
+        break;
+      case ")":
+        tokens.push(new Token(lexemes.shift(), TokenType.CloseParen));
+        break;
+      case "+":
+      case "-":
+      case "*":
+      case "/": {
+        tokens.push(new Token(lexemes.shift(), TokenType.BinaryOperator));
+        break;
+      }
+      case "=":
+        tokens.push(new Token(lexemes.shift(), TokenType.Assignment));
+        break;
+
+      /*
+       * HANDLING MULTI-CHARACTER TOKENS
+       */
+      default: {
+      }
     }
   }
+
+  return tokens;
 }
