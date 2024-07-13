2e0389468440441660410acad9d3d447dd2c798b
seniorsam9 <rxrld050@hotmail.com>
2024-07-13 23:13:59 +0300

    tokenizer

--- src/Lexer/Tokenizer.ts
+++ src/Lexer/Tokenizer.ts
@@ -1,3 +1,4 @@
+import { KEYWORDS } from "./Keywords";
 import { Token } from "./Token";
 import { TokenType } from "./TokenType";
 
@@ -33,9 +34,53 @@ export function tokenize(expr: string): Array<Token> {
        * HANDLING MULTI-CHARACTER TOKENS
        */
       default: {
+        // building number token
+        if (isInt(lexemes[0])) {
+          let num = "";
+          while (lexemes.length > 0 && isInt(lexemes[0])) {
+            num += lexemes.shift();
+          }
+          tokens.push(new Token(num, TokenType.Number));
+        }
+        // test again keywords
+        else if (isAlpha(lexemes[0])) {
+          let identifier = "";
+          while (lexemes.length > 0 && isAlpha(lexemes[0])) {
+            identifier += lexemes.shift();
+          }
+          const reserved = KEYWORDS[identifier];
+          if (reserved === undefined) {
+            tokens.push(new Token(identifier, TokenType.Identifier));
+          } else {
+            tokens.push(new Token(identifier, reserved));
+          }
+        } else if (isSkippableLexeme(lexemes[0])) {
+          // move to enxt character
+          lexemes.shift();
+        }
+        // will be more detailed in future
+        else {
+          console.warn("Unrecognized character at: ", lexemes[0]);
+          process.exit(0);
+        }
+        break;
       }
     }
   }
-
+  tokens.push(new Token("EOF", TokenType.EOF));
   return tokens;
 }
+
+const isAlpha = (lexeme: string): boolean => {
+  return lexeme.toUpperCase() !== lexeme.toLowerCase();
+};
+
+// to be tested
+const isInt = (lexeme: string): boolean => {
+  if (lexeme.length !== 1) return false;
+  return !isNaN(Number(lexeme));
+};
+
+const isSkippableLexeme = (lexeme: string): boolean => {
+  return lexeme === " " || lexeme === "\n" || lexeme === "\t";
+};
